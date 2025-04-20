3151fb8c234dc0428503fcbfaf6d4a1c2f4f2928
Grugus <evan.jackson251@gmail.com>
2025-04-20 14:05:36 -0400

    Added Data Attachments for players

---
+++ src/main/java/net/schnozz/supergoober/attachment/ModDataAttachment.java
@@ -0,0 +1,21 @@
+package net.schnozz.supergoober.attachment;
+
+import com.mojang.serialization.Codec;
+import net.neoforged.neoforge.attachment.AttachmentType;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+import net.schnozz.supergoober.GooberMod;
+
+import java.util.function.Supplier;
+
+public class ModDataAttachment {
+    public static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES = DeferredRegister.create(NeoForgeRegistries.ATTACHMENT_TYPES, GooberMod.MODID);
+
+    // Serialization via codec
+    public static final Supplier<AttachmentType<Integer>> HEALTH_NEEDED = ATTACHMENT_TYPES.register(
+            "needed_health", () -> AttachmentType.builder(() -> 0).serialize(Codec.INT).copyOnDeath().build()
+    );
+
+
+
+}
---
+++ src/main/java/net/schnozz/supergoober/lifesteal/lifeevents/LifeEvents.java
@@ -0,0 +1,48 @@
+package net.schnozz.supergoober.lifesteal.lifeevents;
+
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.schnozz.supergoober.GooberMod;
+import org.checkerframework.checker.units.qual.A;
+
+import static net.schnozz.supergoober.attachment.ModDataAttachment.HEALTH_NEEDED;
+
+@EventBusSubscriber(modid = GooberMod.MODID, bus = EventBusSubscriber.Bus.GAME)
+public class LifeEvents {
+    @SubscribeEvent
+    public static void onLivingDeath(LivingDeathEvent event)
+    {
+        if(event.getEntity() instanceof Player player && event.getSource().getDirectEntity() instanceof Player p && p.getTags().contains("lifestealer"))
+        {
+            p.getData(HEALTH_NEEDED);
+            p.getAttribute(Attributes.MAX_HEALTH).addOrReplacePermanentModifier(new AttributeModifier(ResourceLocation.fromNamespaceAndPath("goobermod", "lifestealer_maxhealth"),Math.min((p.getData(HEALTH_NEEDED) + 4), 20), AttributeModifier.Operation.ADD_VALUE)); // sets the modifier
+            p.setData(HEALTH_NEEDED, Math.min((p.getData(HEALTH_NEEDED) + 4), 20)); //sets the tag on the player to the new health addition value
+        }
+
+        if(event.getEntity() instanceof Player p && event.getSource().getDirectEntity() instanceof Player player && p.getTags().contains("lifestealer"))
+        {
+            p.getData(HEALTH_NEEDED);
+            p.setData(HEALTH_NEEDED, Math.max((p.getData(HEALTH_NEEDED) - 2), -10 ) ); //sets the tag on the player to the new health addition value
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event)
+    {
+        if(event.getEntity().getTags().contains("lifestealer") && event.getEntity().hasData(HEALTH_NEEDED))
+        {
+            Player p = event.getEntity();
+            p.getData(HEALTH_NEEDED);
+            p.getAttribute(Attributes.MAX_HEALTH).addOrReplacePermanentModifier(new AttributeModifier(ResourceLocation.fromNamespaceAndPath("goobermod", "lifestealer_maxhealth"),p.getData(HEALTH_NEEDED), AttributeModifier.Operation.ADD_VALUE)); // sets the modifier
+
+        }
+    }
+}
